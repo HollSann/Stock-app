cb55e3556ce12ad693ae7ae715f041c22fe5b5c5
HollSann <santiago_hollmann@outlook.com>
2022-05-23 20:42:02 -0300

    Le borré el mundo

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express()
 
 app.get('/', (req, res) => {
     console.log("Petición recibida")
-    res.send("<h1>Hola mundo </h1>")
+    res.send("<h1>Hola </h1>")
 })
 
 app.listen(4000,()=>{
