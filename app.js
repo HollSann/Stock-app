a489204efa3666cd3e0bbd5fa38abcd582c2081c
HollSann <santiago_hollmann@outlook.com>
2022-05-23 20:08:10 -0300

    nuestro primer commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) => {
+    console.log("Petición recibida")
+    res.send("<h1>Hola mundo </h1>")
+})
+
+app.listen(4000,()=>{
+    console.log("Escuchando en el puerto 4000")
+})
