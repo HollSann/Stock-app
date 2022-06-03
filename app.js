65a3321ad6a715eb0c0bb195f58773560b588ece
HollSann <santiago_hollmann@outlook.com>
2022-06-02 22:54:26 -0300

    Conexión con la base de datosMongoDB

--- app.js
+++ app.js
@@ -7,16 +7,17 @@ const app = express()
 
 //Conección con  la base de datos de MongoDB
 mongoose
-    .connect(`mongodb+srv://Hollsann:${process.env.MONGO_DB_PASS}@development.ngqbc.mongodb.net/Stock-App?retryWrites=true&w=majority`
-    ).then((result) => console.log('Conección exitosa a la BBDD'))
+    .connect(`mongodb+srv://Hollsann:${process.env.MONGO_DB_PASS}@development.ngqbc.mongodb.net/Stock-App?retryWrites=true&w=majority`)
+    .then((result) => console.log('Conección exitosa a la BBDD'))
     .catch((err) => console.log(err))
 
 //esquema para que mongoose chequee antes de almacenr el contenido a una colección
-const productSchema = mongoose.Schema({  //Dentro de este método de mongoose recibe es un objeto de configuración
-    //con la clausula require se asegura que el campo debe ser obligatoriamente de un tipo específico
-    name: { type: String, require: true },
-    price: { type: Number, require: true },
-},
+const productSchema = mongoose.Schema(
+    {  //Dentro de este método de mongoose recibe es un objeto de configuración
+        //con la clausula require se asegura que el campo debe ser obligatoriamente de un tipo específico
+        name: { type: String, require: true },
+        price: { type: Number, require: true },
+    },
     { timestamps: true } //Automaticamente cuando se cree un registro, se crearan dos campos más, el de created add(fecha en que se crea tal registro) y el update add(cuando se actualizó el registro)
 )
 //Creación de modelo para poder interactuar con la colección a través de un montón de método que provee mongoose
@@ -39,7 +40,7 @@ app.use(express.static(path.join(__dirname, 'public'))) //Ruta donde se encuentr
 
 
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT
 
 app.listen(PORT, () => {
     console.log(`Escuchando por el puerto ${PORT}`)
